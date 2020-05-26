f4d4d18491ff43f6c12b3d134084785fe1b65893
Elena Perrone <elenperrone11@gmail.com>
2020-05-26 10:25:23 +0200

    add ingr

--- src/Burger/Burger.js
+++ src/Burger/Burger.js
@@ -3,16 +3,23 @@ import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 import "./Burger.css";
 
 const Burger = (props) => {
-  let transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((el, ind) => {
-     return <BurgerIngredient key={igKey + ind} type={igKey}/>;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((el, ind) => {
+        return <BurgerIngredient key={igKey + ind} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
+    if (transformedIngredients.length === 0) {
+       transformedIngredients = <p>Please start adding ingredients</p>
+    }
   return (
     <div className="burger  ">
       <BurgerIngredient type="bread-top" />
-     {transformedIngredients}
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
